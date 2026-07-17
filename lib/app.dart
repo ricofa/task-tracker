11d461e28ca8c217b2f4d1210eb8d7a40ae699d9
ricofa <ricof.afandi@gmail.com>
2026-07-17 10:27:42 +0700

    feat: register task provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'features/tasks/providers/task_provider.dart';
+import 'features/tasks/repositories/task_repository.dart';
 
 class SimpleTaskTrackerApp extends StatelessWidget {
   const SimpleTaskTrackerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Task Tracker',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Simple Task Tracker'),
+    return ChangeNotifierProvider(
+      create: (_) => TaskProvider(repository: TaskRepository()),
+      child: MaterialApp(
+        title: 'Simple Task Tracker',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+          useMaterial3: true,
+        ),
+        home: const Scaffold(
+          body: Center(
+            child: Text('Simple Task Tracker'),
+          ),
         ),
       ),
     );
